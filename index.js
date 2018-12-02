e7769e791df03a8aba2a730b0bbead9321fac9ee
Ben Burrell <benjaminjburrell@gmail.com>
2018-12-01 23:39:18 -0700

    Added the home landing page and the registration page. Neither page currently interacts with the database.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/home.html'))
