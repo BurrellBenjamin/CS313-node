cade2a99bd5ae9450233d5854412b0f544ee75e5
Ben Burrell <benjaminjburrell@gmail.com>
2018-12-18 21:47:10 -0700

    Adding in some database functionality

--- controllers/inboxControllers.js
+++ controllers/inboxControllers.js
@@ -5,7 +5,7 @@ const db_url = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: db_url})
 function loadInbox(user, callback){
     console.log("Loading messages for: " + user);
-    sql = "select sourceUser, dateSent, message from messages where recievingUser = (select userID from users where username = $req.session.user)";    
+    sql = "select sourceUser, dateSent, message from messages where recievingUser = (select userID from users where username = " + user + ")";    
     
     pool.query(sql, function(err, db_results){
        if(err){
