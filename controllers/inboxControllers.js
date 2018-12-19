291f691ff351072bd4391a4507508a8079e2a311
Ben Burrell <benjaminjburrell@gmail.com>
2018-12-18 21:49:01 -0700

    Adding in some database functionality

--- controllers/inboxControllers.js
+++ controllers/inboxControllers.js
@@ -3,11 +3,11 @@ const {Pool} = require('pg')
 const db_url = process.env.DATABASE_URL;
 
 const pool = new Pool({connectionString: db_url})
-function loadInbox(user, callback){
+function loadInbox(user){
     console.log("Loading messages for: " + user);
     sql = "select sourceUser, dateSent, message from messages where recievingUser = (select userID from users where username = " + user + ")";    
     
-    pool.query(sql, function(err, db_results){
+    return pool.query(sql, function(err, db_results){
        if(err){
            console.log("Failure to retrieve messages");
        }
@@ -18,7 +18,7 @@ function loadInbox(user, callback){
                success:true,
                list:db_results.rows
            };
-           callback(null, results);
+           return results;
        }
     });
     
