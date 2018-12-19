d6342eb157893b5178e2952f00aa95d33ac34bc9
Ben Burrell <benjaminjburrell@gmail.com>
2018-12-18 20:46:07 -0700

    Adding in some database functionality

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const pool = new Pool({connectionString: db_url})
 
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, ''))
 app.set('view engine', 'ejs')
 app.engine('html', require('ejs').renderFile)
